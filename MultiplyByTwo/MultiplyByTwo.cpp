98d347cee016fdcfe218d08a78f06f5ec17eb364
andre <andreas.m4063@gmail.com>
2022-07-11 13:55:43 +0200

    add() function in seperate file

--- MultiplyByTwo/MultiplyByTwo.cpp
+++ MultiplyByTwo/MultiplyByTwo.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <regex>
 #include <string>
+
 //#include <sstream> // this will allow you to use stringstream in your program
 using namespace std;
+
+bool doCheckForLetters(const::string value) {
+    return regex_search(value, regex("\\d+"));
+}
+
+int add(int x, int y); // needed so MultiplyByTwo.cpp knows that add() is a function defined elsewhere
 
 int main() {
     //create a stringstream object, to input/output strings
     //stringstream ss;
     bool running = true;
+
+    int value = add(5, 9);
+    cout << value << endl;
 
     while (running) {
         string inputValue = "";
@@ -16,7 +26,7 @@ int main() {
         cout << "Input value is:" << inputValue << endl;
         if (inputValue != "quit") {
             int i = 0;
-            if (regex_search(inputValue, regex("\\d+"))) {
+            if (doCheckForLetters(inputValue)) {
                 i = stoi(inputValue);
                 int result = i * 2;
                 cout << "Result: " << result << endl;
@@ -29,7 +39,9 @@ int main() {
         }        
     }    
 }
+
 /*
 bool is_digits(const std::string& str){
     return str.find_first_not_of("0123456789") == std::string::npos;
 }*/
+
